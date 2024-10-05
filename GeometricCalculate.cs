e67b6f55bfadf32bf0c0441d3775bc5ee023dc10
Emre <sabentoki06@hotmail.com>
2024-10-05 03:36:49 +0300

    Add files via upload

---
+++ GeometricCalculate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Kullanıcıdan 2 sayı istiyorum.
+        Console.Write("Birinci sayiyi girin: ");
+        double sayi1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("İkinci sayiyi girin: ");
+        double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+        // Kullanıcıdan aldğımız 2 sayının geometrik ortalamasını hesaplıyorum.
+        double geometrikOrtalama = Math.Sqrt(sayi1 * sayi2);
+
+        // Geometrik hesaplamasını yaptığımız sayıların sonucu ekrana yazdırıyorum.
+        Console.WriteLine("Geometrik Ortalama: " + geometrikOrtalama);
+    }
+}
