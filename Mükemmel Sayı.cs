495bc46a9a0a272c0a39bdfe56cb0f001f2b048e
Emre <sabentoki06@hotmail.com>
2024-11-20 21:20:42 +0300

    Add files via upload

---
+++ Mükemmel Sayı.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program
+{
+ static void Main()
+ {
+ Console.WriteLine("Sayı Değerinizi Giriniz:");
+ int sayi = int.Parse(Console.ReadLine());
+ if (MükemmelSayiMi(sayi))
+ {
+ Console.WriteLine($"{sayi} Bir Mükemmel Sayıdır.");
+ }
+ else
+ {
+ Console.WriteLine($"{sayi} Bir Mükemmel Sayı Değildir");
+ }
+ }
+ static bool MükemmelSayiMi(int sayi)
+ {
+ if (sayi <= 1) return false;
+ int toplam = 0;
+ for (int i = 1; i < sayi; i++)
+ {
+ if (sayi % i == 0)
+ {
+ toplam += i;
+ }
+ }
+ return toplam == sayi;
+ }
+}
