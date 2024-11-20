e03315729c2b13a9d4df9a1086481f4ffef9f49b
Emre <sabentoki06@hotmail.com>
2024-11-20 21:34:42 +0300

    Add files via upload

---
+++ Üçgen.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.Write("a değerini yazın: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("b değerini yazın: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("c değerini yazın: ");
+        int c = Convert.ToInt32(Console.ReadLine());
+
+       
+        int toplam = a + b + c;
+
+       
+        if (toplam < 180)
+        {
+            Console.WriteLine("Üçgen değil");
+        }
+        else if (toplam > 180)
+        {
+            Console.WriteLine("Üçgen değil");
+        }
+        else
+        {
+            Console.WriteLine("Üçgen");
+        }
+
+        
+        Console.WriteLine("Program sonlandı.");
+    }
+}
