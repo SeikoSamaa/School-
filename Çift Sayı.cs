b2b31ad9e31b8318de587adb26e74eb460cf6583
Emre <sabentoki06@hotmail.com>
2024-11-20 21:31:42 +0300

    Add files via upload

---
+++ Çift Sayı.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+class Sınav
+{
+    static void Main()
+    {
+        int sayac =0;
+        int adet =0;
+        Console.WriteLine("1. Sayi Degerini Giriniz");
+        int sayi1 = int.Parse (Console.ReadLine());
+        
+        Console.WriteLine("2. Sayi Degerini Giriniz");
+        int sayi2 = int.Parse(Console.ReadLine());
+        
+        for (int i = sayi1; i < sayi2; i++)
+         if (i%2==0)
+         {
+            Console.WriteLine(i);
+            sayac=i;
+            adet=i;
+         }
+         Console.WriteLine(sayac);
+         Console.WriteLine("Toplam adet: "+adet/2);
+
+    }
+}   
+
+ 
