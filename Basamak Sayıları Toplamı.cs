35c5ffa4b09ca628ebcfd4103fe0fce3ce580e22
Emre <sabentoki06@hotmail.com>
2024-11-20 21:26:46 +0300

    Add files via upload

---
+++ Basamak Sayıları Toplamı.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Bir sayı girin: ");
+        int sayi = int.Parse(Console.ReadLine());
+        int toplam = 0;
+
+        for (int i = 1; i <= sayi; i++)
+        {
+            toplam += i;
+        }
+
+        Console.WriteLine($"1'den {sayi}'ye kadar olan sayıların toplamı: {toplam}");
+    }
+}
