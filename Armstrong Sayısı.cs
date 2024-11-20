51f713f0975b6d8ac8a9b42e5d240dd6794c31c9
Emre <sabentoki06@hotmail.com>
2024-11-20 21:18:41 +0300

    Add files via upload

---
+++ Armstrong Sayısı.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Bir sayı girin: ");
+        int sayi = int.Parse(Console.ReadLine());
+
+        int basamakSayisi = sayi.ToString().Length;
+
+        int toplam = 0;
+        int geçiciSayi = sayi;
+
+        while (geçiciSayi > 0)
+        {
+            int basamak = geçiciSayi % 10;
+            toplam += (int)Math.Pow(basamak, basamakSayisi);
+            geçiciSayi /= 10;
+        }
+
+        if (toplam == sayi)
+        {
+            Console.WriteLine($"{sayi} bir Armstrong sayısıdır");
+        }
+        else
+        {
+            Console.WriteLine($"{sayi} bir Armstrong sayısı değildir");
+        }
+    }
+}
